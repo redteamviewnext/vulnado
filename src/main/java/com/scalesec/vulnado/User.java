213ae63bf6283484e9729a5a3f1878a940f69173
redteamviewnext <163271880+redteamviewnext@users.noreply.github.com>
2025-01-09 02:21:32 +0100

    Update User.java

--- src/main/java/com/scalesec/vulnado/User.java
+++ src/main/java/com/scalesec/vulnado/User.java
@@ -1,7 +1,6 @@
 package com.scalesec.vulnado;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement; // Asegúrate de importar PreparedStatement
 import java.sql.ResultSet;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.JwtParser;
@@ -37,30 +36,28 @@ public class User {
   }
 
   public static User fetch(String un) {
-      PreparedStatement pstmt = null;
-      User user = null;
-      try {
-          Connection cxn = Postgres.connection();
-          System.out.println("Opened database successfully");
-  
-          // Usar consultas preparadas para evitar inyección SQL
-          String query = "SELECT * FROM users WHERE username = ? LIMIT 1";
-          pstmt = cxn.prepareStatement(query);
-          pstmt.setString(1, un); // Establecer el valor de "un" de manera segura
-  
-          ResultSet rs = pstmt.executeQuery();
-  
-          if (rs.next()) {
-              String user_id = rs.getString("user_id");
-              String username = rs.getString("username");
-              String password = rs.getString("password");
-              user = new User(user_id, username, password);
-          }
-          cxn.close();
-      } catch (Exception e) {
-          e.printStackTrace();
-          System.err.println(e.getClass().getName() + ": " + e.getMessage());
+    Statement stmt = null;
+    User user = null;
+    try {
+      Connection cxn = Postgres.connection();
+      stmt = cxn.createStatement();
+      System.out.println("Opened database successfully");
+
+      String query = "select * from users where username = '" + un + "' limit 1";
+      System.out.println(query);
+      ResultSet rs = stmt.executeQuery(query);
+      if (rs.next()) {
+        String user_id = rs.getString("user_id");
+        String username = rs.getString("username");
+        String password = rs.getString("password");
+        user = new User(user_id, username, password);
       }
+      cxn.close();
+    } catch (Exception e) {
+      e.printStackTrace();
+      System.err.println(e.getClass().getName()+": "+e.getMessage());
+    } finally {
       return user;
+    }
   }
 }
