85509a3549936e546dc16669aa6d0aa404625e18
redteamviewnext <163271880+redteamviewnext@users.noreply.github.com>
2025-01-09 17:13:13 +0100

    Update User.java

--- src/main/java/com/scalesec/vulnado/User.java
+++ src/main/java/com/scalesec/vulnado/User.java
@@ -36,28 +36,32 @@ public class User {
     }
   }
   public static User fetch(String un) {
-    Statement stmt = null;
     User user = null;
     try {
-      Connection cxn = Postgres.connection();
-      stmt = cxn.createStatement();
-      System.out.println("Opened database successfully");
+        Connection cxn = Postgres.connection();
+        System.out.println("Opened database successfully");
 
-      String query = "select * from users where username = '" + un + "' limit 1";
-      System.out.println(query);
-      ResultSet rs = stmt.executeQuery(query);
-      if (rs.next()) {
-        String user_id = rs.getString("user_id");
-        String username = rs.getString("username");
-        String password = rs.getString("password");
-        user = new User(user_id, username, password);
-      }
-      cxn.close();
+        // Usar PreparedStatement para prevenir inyección SQL
+        String query = "SELECT * FROM users WHERE username = ? LIMIT 1";
+        PreparedStatement pstmt = cxn.prepareStatement(query);
+        pstmt.setString(1, un);
+
+        ResultSet rs = pstmt.executeQuery();
+        if (rs.next()) {
+            String user_id = rs.getString("user_id");
+            String username = rs.getString("username");
+            String password = rs.getString("password");
+            user = new User(user_id, username, password);
+        }
+
+        // Cerrar recursos
+        rs.close();
+        pstmt.close();
+        cxn.close();
     } catch (Exception e) {
-      e.printStackTrace();
-      System.err.println(e.getClass().getName()+": "+e.getMessage());
-    } finally {
-      return user;
+        e.printStackTrace();
+        System.err.println(e.getClass().getName() + ": " + e.getMessage());
     }
-  }
+
+    return user;
 }
